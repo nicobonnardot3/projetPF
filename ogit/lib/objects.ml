aaa875a3e49ec676d479f764288b7a8cdaaf1454
nicobonnardot3 <nicobonnardot3@gmail.com>
2022-11-08 16:48:57 +0100

    small prog

--- ogit/lib/objects.ml
+++ ogit/lib/objects.ml
@@ -18,18 +18,25 @@ let read_text_object _h: Digest.t = Core.In_channel.read_all (".ogit/objects/" ^
 
 let store_work_directory () = failwith "TODO ( store_work_directory )"
 
-let read_directory_object _h =
+let rec read_directory_object _h =
   let dataString = read_text_object _h in
   let splitData = String.split_on_char '\n' dataString in
-  let rec createDirObj data = match data with
+  let rec createDirObj data = 
+    match data with
     | [] -> Text ""
-    | [txt] -> 
+    | [txt] ->
         let txtData = String.split_on_char ';' txt in
         if (List.nth txtData 1) = "t" then Text (List.nth txtData 2)
-        else Directory (List.hd txtData, true, (List.nth txtData 2), createDirObj ( String.split_on_char '\n'  ) )
+        else Directory ([List.hd txtData, true, (List.nth txtData 2), read_directory_object ( List.nth txtData 2 )] )
+    | hd::tl ->
+        let txtData = String.split_on_char ';' hd in
+        if (List.nth txtData 1) = "t" then (Text (List.nth txtData 2))::(createDirObj(tl))
+        else (Directory ([List.hd txtData, true, (List.nth txtData 2), read_directory_object ( List.nth txtData 2 )]))::(createDirObj(tl))
+  in
+  Text ""
   
 let clean_work_directory () = failwith "TODO ( clean_work_directory )"
 
-let restore_work_directory _obj = failwith "TODO ( restore_work_directory )" 
+let restore_work_directory _obj = failwith "TODO ( restore_work_directory )"
 
 let merge_work_directory_I _obj = failwith "TODO ( merge_work_directory_I )"
