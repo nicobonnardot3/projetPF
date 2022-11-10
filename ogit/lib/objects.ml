1dfd1cdb5edffdbe25dc4f4c7214f0833ea4f8c6
nicobonnardot3 <nicobonnardot3@gmail.com>
2022-11-10 11:03:08 +0100

    fixed

--- ogit/lib/objects.ml
+++ ogit/lib/objects.ml
@@ -24,22 +24,18 @@ let clean_work_directory () =
   
   let rec clearDir dir =
     let currentDirArray = Sys.readdir dir in 
-
-    for i = 0 to ((Array.length currentDirArray)-1) do 
+    for i = 0 to (Array.length currentDirArray) - 1 do
       let currentFile = currentDirArray.(i) in 
       let firstChar = String.get currentFile 0 in
-
-      if(firstChar <> '.') then 
-
-        if !(Sys.is_directory currentFile) then Sys.remove currentFile
-        else try Sys.rmdir currentFile with | _ -> clearDir dir ^ "/" ^ currentFile
-
+      if firstChar <> '.' then begin
+        if Sys.is_directory currentFile = false then
+          Sys.remove currentFile
+        else
+          try Sys.command ("rm -r " ^currentFile) |> ignore with | _ -> clearDir (dir ^ "/" ^ currentFile)
+      end
+    done
+  in
   clearDir "./"
-    
-  
-;;
-
-
 
 let restore_work_directory _obj = failwith "TODO ( restore_work_directory )" 
 
