d4698b245fe0bf2158838474142cec0f9f7e6d22
DorianFornali <dorian.fornali@outlook.fr>
2022-11-15 14:58:21 +0100

    .

--- ogit/lib/objects.ml
+++ ogit/lib/objects.ml
@@ -114,3 +114,11 @@ let merge_work_directory_I _obj =
   Si fichier présent dans x mais pas ici, on l'ajoute ici / si il existe et a le meme contenu rien ne se passe / si il existe dans les 2
   et a pas le même contenu -> on crée deux versions du fichiers -> blabla.cl (local version) et blabla.cr (remote version)   
   *)
+
+  let currentDirArray = Sys.readdir in
+  let auxiliaire _obj =
+    for i = 0 to (currentDirArray.length-1) do
+      match _obj with
+      | Text(contenu) -> begin 
+        if !(currentDirArray.(i).is_directory)
+
