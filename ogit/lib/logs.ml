37da3a95f32f3a10b21f2d9de5dc4193a6a84eb7
nicobonnardot3 <nicobonnardot3@gmail.com>
2022-11-15 11:57:59 +0100

    create some functions

--- ogit/lib/logs.ml
+++ ogit/lib/logs.ml
@@ -5,13 +5,26 @@ type commit = {
     content : Digest.t
 }
     
-let date_fm _d = failwith "TODO"
+let date_fm _d = 
+    let time = Unix.localtime _d in
+    Printf.sprintf "%02d:%02d:%02d-%02d-%02d-%04d" time.tm_hour time.tm_min time.tm_sec time.tm_mday (time.tm_mon + 1) (time.tm_year + 1900) 
+        
+let set_head _l =
+    let content = (List.fold_left (fun x y -> x ^ ";" ^ y) "" _l) in
+    let len = String.length content in
+    let treatOc ic = Out_channel.output_string ic (String.sub content 1 (len - 1) ) in
+    Out_channel.with_open_text ".ogit/HEAD" treatOc
+    
 
-let set_head _l = failwith "TODO"
+let get_head () = 
+    let treatIc ic = In_channel.input_all ic in
+    In_channel.with_open_text ".ogit/HEAD" treatIc
 
-let get_head () = failwith "TODO" 
-
-let make_commit _s  _h =  failwith "TODO"
+let make_commit _s _h = 
+    let head = get_head () in
+    let parents = String.split_on_char ';' head in
+    let date = Unix.time () in
+    {parents; date; message = _s; content = _h}
 
 let init_commit () = failwith "TODO"
 
