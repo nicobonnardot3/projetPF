a4c99c7d1a7acbfda8e765ec01e8ec313c56aab1
nicobonnardot3 <nicobonnardot3@gmail.com>
2022-11-17 20:09:24 +0100

    finished init command

--- ogit/lib/commands.ml
+++ ogit/lib/commands.ml
@@ -1,7 +1,15 @@
 (** fichier commands.ml **)
 (** fonctions représentant les commandes ogit **)
 
-let ogit_init () =  failwith "TODO"
+let ogit_init () = 
+  if Sys.file_exists ".ogit" = false then begin
+  Sys.command "mkdir .ogit" |> ignore;
+  Sys.command "mkdir .ogit/objects" |> ignore;
+  Sys.command "mkdir .ogit/logs" |> ignore;
+  Logs.set_head [Objects.store_work_directory ()];
+  Logs.store_commit (Logs.init_commit ()) |> ignore
+  end
+  else failwith "Already initialized"
 
 let ogit_commit _msg = failwith "TODO"
 
