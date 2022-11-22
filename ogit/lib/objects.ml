7d6dc2bef740e025ad1ef4b652fd4726b2036a6d
nicobonnardot3 <nicobonnardot3@gmail.com>
2022-11-22 15:03:13 +0100

    tmp

--- ogit/lib/objects.ml
+++ ogit/lib/objects.ml
@@ -25,7 +25,7 @@ let rec hash _obj =
   match _obj with
   | Text text -> Digest.string text
   | Directory ([]) -> ""
-  | Directory ((name, isDir, _, t)::tl) -> 
+  | Directory ((name, isDir, _, t)::tl) ->
     if isDir then Digest.string (name ^ ";d;" ^ (hash t) ^ (hash (Directory(tl))))
     else Digest.string (name ^ ";t;" ^ (hash t) ^ (hash (Directory(tl))))
 
@@ -33,10 +33,7 @@ let is_known _h = Sys.file_exists (".ogit/objects/" ^ _h)
 
 let store_object _obj = failwith "TODO ( store_object )"
 
-let read_text_object _h =
-  print_string (Digest.to_hex _h);
-  print_newline();
-  read_all (".ogit/objects/" ^ (Digest.to_hex _h))
+let read_text_object _h = read_all (".ogit/objects/" ^ (Digest.to_hex _h))
 
 let store_work_directory () : Digest.t =
   let rec treatCurrentDir dir : Digest.t =
@@ -68,7 +65,7 @@ let store_work_directory () : Digest.t =
   treatCurrentDir "../repo"
 
 let rec read_directory_object _h =
-  let dataString = read_text_object _h in
+  let dataString = read_text_object (Digest.from_hex _h) in
   let splitData = String.split_on_char '\n' dataString in
   let rec createDirObj data = match data with
     | [] -> [("", false, Digest.string "", Text "")]
@@ -77,21 +74,21 @@ let rec read_directory_object _h =
         let fileName = List.hd (String.split_on_char '\r' (List.nth txtData 2)) in
         let fileContent = read_text_object (Digest.from_hex fileName) in
         if (List.nth txtData 1) = "t" then [(List.hd txtData, false, hash (Text (fileContent)), Text (fileContent))]
-        else [(List.hd txtData, true, (List.nth txtData 2), read_directory_object ( Digest.from_hex (List.nth txtData 2 )))]
+        else [(List.hd txtData, true, Digest.from_hex (List.nth txtData 2), read_directory_object (List.nth txtData 2 ))]
     | hd::tl ->
         let txtData = String.split_on_char ';' hd in
         let fileName = List.hd (String.split_on_char '\r' (List.nth txtData 2)) in
         let fileContent = read_text_object (Digest.from_hex fileName) in
         if (List.nth txtData 1) = "t" then (List.hd txtData, false, hash (Text (fileContent)), Text (fileContent))::(createDirObj(tl))
-        else (List.hd txtData, true, (List.nth txtData 2), read_directory_object ( Digest.from_hex (List.nth txtData 2) ))::(createDirObj(tl))
+        else (List.hd txtData, true, Digest.from_hex (List.nth txtData 2), read_directory_object (List.nth txtData 2))::(createDirObj(tl))
   in
   Directory (List.rev (createDirObj splitData))
 
-let clean_work_directory () = 
+let clean_work_directory () =
   let rec clearDir dir =
-    let currentDirArray = Sys.readdir dir in 
+    let currentDirArray = Sys.readdir dir in
     for i = 0 to (Array.length currentDirArray) - 1 do
-      let currentFile = currentDirArray.(i) in 
+      let currentFile = currentDirArray.(i) in
       let firstChar = String.get currentFile 0 in
       if firstChar <> '.' then begin
         if Sys.is_directory currentFile = false then
